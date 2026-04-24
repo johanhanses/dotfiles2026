4d7611755b6821c0d46b10be649ef1838a33c9cb
Johan Hanses <johanhanses@gmail.com>
2026-04-24 10:11:22 +0200

    nvim: replace LazyVim with from-scratch config
    Archive the LazyVim-based config as nvim.lazyvim/. New nvim/ uses
    lazy.nvim as a package manager only (not the LazyVim distribution).
    
    13 plugins: treesitter, lspconfig+mason, nvim-cmp, conform, gitsigns,
    which-key, snacks (picker), lualine, vim-tmux-navigator, claudecode,
    no-neck-pain. No neo-tree (use :Ex), no colorscheme plugin (Neovim
    default). Cherry-picked options/keymaps/autocmds from LazyVim.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,21 +1,58 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
--- Disable conceallevel for json, jsonc, and markdown files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "jsonc", "markdown" },
+autocmd("TextYankPost", {
+  group = augroup("highlight_yank", {}),
   callback = function()
-    vim.wo.conceallevel = 0
+    vim.hl.on_yank()
   end,
 })
 
--- Disable completions, diagnostics, and spell checking for markdown files
-vim.api.nvim_create_autocmd("FileType", {
+autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime", {}),
+  command = "checktime",
+})
+
+autocmd("VimResized", {
+  group = augroup("resize_splits", {}),
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
+
+autocmd("BufReadPost", {
+  group = augroup("last_loc", {}),
+  callback = function(event)
+    local mark = vim.api.nvim_buf_get_mark(event.buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(event.buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+autocmd("FileType", {
+  group = augroup("close_with_q", {}),
+  pattern = { "help", "man", "qf", "checkhealth", "notify", "lspinfo" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = event.buf, silent = true })
+  end,
+})
+
+autocmd("FileType", {
+  group = augroup("json_md_conceal", {}),
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
+autocmd("FileType", {
+  group = augroup("md_text_settings", {}),
   pattern = { "markdown", "text" },
   callback = function()
-    vim.b.blink_cmp_enabled = false
-    vim.b.copilot_enabled = false
-    vim.diagnostic.enable(false, { bufnr = 0 })
-    vim.wo.spell = false
+    vim.opt_local.spell = false
+    vim.b.completion = false
   end,
 })
